7dd82ad63184cab12a52be4245d26e3a53c9a257
shakesVan <779249320@qq.com>
2018-12-10 23:21:05 +0800

    init

---
+++ MyFramework2/MyFramework2/MyFramework2.h
@@ -0,0 +1,19 @@
+//
+//  MyFramework2.h
+//  MyFramework2
+//
+//  Created by 范摇 on 2018/12/10.
+//  Copyright © 2018 范摇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyFramework2.
+FOUNDATION_EXPORT double MyFramework2VersionNumber;
+
+//! Project version string for MyFramework2.
+FOUNDATION_EXPORT const unsigned char MyFramework2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFramework2/PublicHeader.h>
+
+
