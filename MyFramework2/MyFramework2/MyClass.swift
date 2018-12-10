15aa8f3d904128551cd0242f670c0c322b245dfc
shakesVan <779249320@qq.com>
2018-12-10 23:26:50 +0800

    add code

---
+++ MyFramework2/MyFramework2/MyClass.swift
@@ -0,0 +1,15 @@
+//
+//  MyClass.swift
+//  MyFramework2
+//
+//  Created by 范摇 on 2018/12/10.
+//  Copyright © 2018 范摇. All rights reserved.
+//
+
+import Foundation
+
+open class MyClass {
+    open func sayHello() {
+        print("MyFramework2 sayHello")
+    }
+}
